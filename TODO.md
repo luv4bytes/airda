d36ff54d1d7c278eb1db1172a0421859858b8240
luv4bytes <61706439+luv4bytes@users.noreply.github.com>
2023-02-02 16:42:47 +0100

    Additive and multiplicative expressions

--- TODO.md
+++
@@ -1,1 +0,0 @@
-- Single file as entry point.
--- src/Parser.hs
+++ src/Parser.hs
@@ -226,9 +226,11 @@ parse tokens fileName = program tokens >>= \nodes -> Right (AST.Root nodes fileN
       where
         tt = Lexer.tokenType
     expression state@(t : r@(_t : ts))
-      | tt t == Lexer.Identifier && tt _t == Lexer.Plus = addExpr ts (op _t) (idExpr t)
+      | tt t == Lexer.Identifier && tt _t `elem` [Lexer.Multiply, Lexer.Divide] = multExpr ts (op _t) (idExpr t)
+      | tt t == Lexer.Identifier && tt _t `elem` [Lexer.Plus, Lexer.Minus] = addExpr ts (op _t) (idExpr t)
       | tt t == Lexer.Identifier = Right (idExpr t, r)
-      | tt t == Lexer.NumericLiteral && tt _t == Lexer.Plus = addExpr ts (op _t) (numExpr t)
+      | tt t == Lexer.NumericLiteral && tt _t `elem` [Lexer.Multiply, Lexer.Divide] = multExpr ts (op _t) (idExpr t)
+      | tt t == Lexer.NumericLiteral && tt _t `elem` [Lexer.Plus, Lexer.Minus] = addExpr ts (op _t) (numExpr t)
       | tt t == Lexer.NumericLiteral = Right (numExpr t, r)
       | otherwise =
           Left
@@ -261,13 +263,67 @@ parse tokens fileName = program tokens >>= \nodes -> Right (AST.Root nodes fileN
                     }
                 )
         addExpr state@(t : r@(_t : ts)) binOp lhs
-          | tt t == Lexer.Identifier && tt _t == Lexer.Plus =
+          | tt t == Lexer.Identifier && tt _t `elem` [Lexer.Plus, Lexer.Minus] =
+              case addExpr ts (op _t) (AST.BinaryExpression lhs binOp (idExpr t)) of
+                Left pe -> Left pe
+                Right (e, s) -> Right (e, s)
+          | tt t == Lexer.Identifier && tt _t `elem` [Lexer.Multiply, Lexer.Divide] =
+              case multExpr ts (op _t) (idExpr t) of
+                Left pe -> Left pe
+                Right (e, s) -> Right (AST.BinaryExpression lhs binOp e, s)
+          | tt t == Lexer.Identifier = Right (AST.BinaryExpression lhs binOp (idExpr t), r)
+          | tt t == Lexer.NumericLiteral && tt _t `elem` [Lexer.Plus, Lexer.Minus] =
+              case addExpr ts (op _t) (AST.BinaryExpression lhs binOp (numExpr t)) of
+                Left pe -> Left pe
+                Right (ae, s) -> Right (ae, s)
+          | tt t == Lexer.NumericLiteral && tt _t `elem` [Lexer.Multiply, Lexer.Divide] =
+              case multExpr ts (op _t) (idExpr t) of
+                Left pe -> Left pe
+                Right (e, s) -> Right (AST.BinaryExpression lhs binOp e, s)
+          | tt t == Lexer.NumericLiteral = Right (AST.BinaryExpression lhs binOp (numExpr t), r)
+          | otherwise =
+              Left
+                ( Error.ParserException
+                    { Error.pexMessage = "Invalid expression '" ++ Lexer.tokenValue t ++ "'.",
+                      Error.pexErrCode = Error.errInvalidToken,
+                      Error.pexLineNum = Just (Lexer.tokenLineNum t),
+                      Error.pexColNum = Just (Lexer.tokenColumn t),
+                      Error.pexFileName = Lexer.fileName t
+                    }
+                )
+
+        -- \| Multiplicative Expression
+        multExpr :: ParserState.ParserState -> AST.TreeNode -> AST.TreeNode -> Either Error.ParserException (AST.TreeNode, ParserState.ParserState)
+        multExpr [] _ _ = Left (Error.ParserExceptionSimple "Syntax error.")
+        multExpr [t] op lhs
+          | tt t == Lexer.Identifier = Right (AST.BinaryExpression lhs op (idExpr t), [])
+          | tt t == Lexer.NumericLiteral = Right (AST.BinaryExpression lhs op (numExpr t), [])
+          | otherwise =
+              Left
+                ( Error.ParserException
+                    { Error.pexMessage = "Invalid expression '" ++ Lexer.tokenValue t ++ "'.",
+                      Error.pexErrCode = Error.errInvalidToken,
+                      Error.pexLineNum = Just (Lexer.tokenLineNum t),
+                      Error.pexColNum = Just (Lexer.tokenColumn t),
+                      Error.pexFileName = Lexer.fileName t
+                    }
+                )
+        multExpr state@(t : r@(_t : ts)) binOp lhs
+          | tt t == Lexer.Identifier && tt _t `elem` [Lexer.Multiply, Lexer.Divide] =
+              case multExpr ts (op _t) (AST.BinaryExpression lhs binOp (idExpr t)) of
+                Left pe -> Left pe
+                Right (e, s) -> Right (e, s)
+          | tt t == Lexer.Identifier && tt _t `elem` [Lexer.Plus, Lexer.Minus] =
               case addExpr ts (op _t) (AST.BinaryExpression lhs binOp (idExpr t)) of
                 Left pe -> Left pe
                 Right (e, s) -> Right (e, s)
           | tt t == Lexer.Identifier = Right (AST.BinaryExpression lhs binOp (idExpr t), r)
-          | tt t == Lexer.NumericLiteral && tt _t == Lexer.Plus =
-              case addExpr ts (op _t) (AST.BinaryExpression lhs binOp (numExpr t)) of
+          | tt t == Lexer.NumericLiteral && tt _t `elem` [Lexer.Multiply, Lexer.Divide] =
+              case multExpr ts (op _t) (AST.BinaryExpression lhs binOp (numExpr t)) of
+                Left pe -> Left pe
+                Right (e, s) -> Right (e, s)
+          | tt t == Lexer.NumericLiteral && tt _t `elem` [Lexer.Plus, Lexer.Minus] =
+              case addExpr ts (op _t) (AST.BinaryExpression lhs binOp (idExpr t)) of
                 Left pe -> Left pe
                 Right (e, s) -> Right (e, s)
           | tt t == Lexer.NumericLiteral = Right (AST.BinaryExpression lhs binOp (numExpr t), r)
